dc9a2e5a900bac7f623f61b003774b2b763c3015
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2022-07-19 02:55:37 +0800

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-testing-87b96',
+    appId: '1:290715116821:web:f715976bfe5aa15699751c',
+    storageBucket: 'angular-testing-87b96.appspot.com',
+    locationId: 'asia-southeast1',
+    apiKey: 'AIzaSyC-5EsiM38budE1s8KA6oLHy6cXW_5nbhU',
+    authDomain: 'angular-testing-87b96.firebaseapp.com',
+    messagingSenderId: '290715116821',
+  },
   production: true
 };
